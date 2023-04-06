df76c9740954ff6704f0d1b5448c81f820091019
ErsanDkc <edikici55@gmail.com>
2023-04-06 17:13:37 +0300

    css was  updated

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -5,10 +5,7 @@ import "./styles.css"
 import { incereasePiece } from '../../redux/Money/moneySlice';
 function Products() {
   const dispatch = useDispatch()
-  const [piece,setPiece] = useState(0)
-  const buyItem = (amount,price,title) => {
-    dispatch(incereasePiece({amount,price,title}))
-  }
+  
   return (
     <section>
       {
@@ -21,8 +18,8 @@ function Products() {
           <div className="price">{item.price}</div>
           <div className="cardControl">
             <button className='red'>Sell</button>
-            <input type="number" value={piece}  onChange={(e) => setPiece(e.target.value)} />
-            <button className='green' onClick={() => buyItem(1,item.price,item.title)}>Buy</button>
+            <input type="number" value={piece}  />
+            <button className='green' >Buy</button>
           </div>
 
         </div>)
