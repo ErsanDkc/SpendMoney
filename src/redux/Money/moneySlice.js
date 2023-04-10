23c2050ac298bb914014f86c37dc6280c866c2bc
ErsanDkc <edikici55@gmail.com>
2023-04-10 10:01:11 +0300

    styles updated

--- src/redux/Money/moneySlice.js
+++ src/redux/Money/moneySlice.js
@@ -1,55 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// export const moneySlice = createSlice({
-//     name: "money",
-//     initialState: {
-//         value: 1000000000,
-//         difference:0,
-//         items:[]
-//     },
-//     reducers: {
-//         buy: (state,action) => {
-//         let {price,amount} = action.payload
-//         state.difference = parseInt(price) *parseInt(amount)
-//         state.value = state.value - state.difference
-        
-//         let addedItem = state.items.find(
-//             (item) => item.title === action.payload.title
-//           );
-//           if (addedItem) {
-//             addedItem.amount += action.payload.amount;
-//           } else {
-//             state.items = [...state.items, action.payload];
-//           }
-        
-//         },
-//         sell: (state,action) => {
-//             let {price,amount} = action.payload
-//             state.difference = parseInt(price) *parseInt(amount)
-//             state.value = state.value + state.difference
-            
-//             let addedItem = state.items.find(
-//                 (item) => item.title === action.payload.title
-//               );
-//               if (addedItem) {
-//                 if (addedItem.amount - action.payload.amount === 0) {
-//                   state.items = state.items.filter(
-//                     (item) => item.title !== addedItem.title
-//                   );
-//                 } else {
-//                   addedItem.amount -= action.payload.amount;
-//                 }
-//               }
-            
-            
-//             }
-//     }
-// })
 
-// export const selectItems = (state) => state.money.items;
-// export const selectValue = (state) => state.money.value;
-// export const {buy,sell} = moneySlice.actions
-// export default moneySlice.reducer
 export const moneySlice = createSlice({
     name: "money",
     initialState: {
